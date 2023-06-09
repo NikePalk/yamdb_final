de7b0010b83525de721634d286e9167033660de4
NikePalk <palkin.nikita.sergeevich@yandex.ru>
2023-06-09 19:21:02 +0300

    redoc

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,11 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
     'django_filters',
-    'api',
-    'reviews',
 ]
 
 MIDDLEWARE = [
