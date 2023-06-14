d57c4494447f9eb5c9e571850997cf4a6d550271
NikePalk <palkin.nikita.sergeevich@yandex.ru>
2023-06-14 16:06:04 +0300

    finality

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,12 +4,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    os.getenv('SECRET_KEY'),
-    'my_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEB')
 
 ALLOWED_HOSTS = ['*']
 
