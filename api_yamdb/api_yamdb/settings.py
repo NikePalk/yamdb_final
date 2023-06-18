71fa7c0c46fc4503ea9c1e3b095c431420fae9bb
NikePalk <palkin.nikita.sergeevich@yandex.ru>
2023-06-18 12:38:04 +0300

    Debug

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = (
     'my_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
