9b77019996d1d3fce85a95fdc55c255b181252bf
NikePalk <palkin.nikita.sergeevich@yandex.ru>
2023-06-20 15:45:17 +0300

    777

--- api_yamdb/reviews/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils import timezone
-
-
-def validate_year(year):
-    current_year = timezone.now().year
-    if year > current_year:
-        raise ValidationError(
-            f'Title не может быть создан позже текущего года ({current_year})')
