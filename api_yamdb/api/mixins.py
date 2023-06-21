8cda756a514d0da1b008c7acbb5b874ffcd10a54
NikePalk <palkin.nikita.sergeevich@yandex.ru>
2023-06-21 17:42:26 +0300

    proba

--- api_yamdb/api/mixins.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import mixins, viewsets
-
-
-class ModelSetCLD(mixins.CreateModelMixin, mixins.ListModelMixin,
-                  mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    pass
